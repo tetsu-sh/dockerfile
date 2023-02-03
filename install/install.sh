f0203044082167460efcd3d3ba52c7efd75ec40b
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2023-02-03 16:52:11 +0900

    add mysql client

--- install/install.sh
+++ install/install.sh
@@ -5,6 +5,7 @@ apt-get update
 apt-get install -y vim build-essential man strace sysstat wget curl
 apt-get install -y zsh
 apt-get install -y iputils-ping net-tools dnsutils
+apt-get install -y mysql-client
 
 
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
